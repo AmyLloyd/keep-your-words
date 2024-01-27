86c96132d9590e4fddb2896f0835a5cd681b24a4
Amy Lloyd <amylloyd30@gmail.com>
2024-01-28 07:57:52 +1030

    update latest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 
+require('.')
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
@@ -27,6 +28,22 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'service-worker.js',
       }), 
+      new WebpackPwaManifest({
+        name: 'Keep your words',
+        short_name: 'words',
+        description: 'Keep your important words!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+      ],
+      }),
       
     ],
 
